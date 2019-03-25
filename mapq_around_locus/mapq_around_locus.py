80e11c21a6d22df0eca773d0e845af6614bfc3b9
Eric Kofman <ericrkofman@gmail.com>
2019-03-25 16:36:45 -0400

    Description

--- mapq_around_locus/mapq_around_locus.py
+++ mapq_around_locus/mapq_around_locus.py
@@ -7,6 +7,30 @@ import pandas as pd
 import seaborn as sns
 import random
 
+"""
+Example usage at single location:
+python mapq_around_locus.py
+--bam_file mother.bam
+--chromosome 20
+--position 16059512
+--output_dir outputs
+
+Example usage at several locations specified in a tab-delimited file with a 'Chromosome' and 'Start' column:
+python mapq_around_locus.py
+--bam_file mother.bam
+--positions_file positions.txt
+--output_dir outputs
+
+Example positions.txt file contents (tab-delimited):
+Chromosome	Start
+20	16059226
+20	16061080
+20	16064664
+20	16067971
+20	16073033
+20	16078668
+"""
+
 
 def mapq_around_locus(bam_file, chromosome, position, window, offset):
     """Get the distribution of quality scores for all reads falling within the window of size 'window' centered around
