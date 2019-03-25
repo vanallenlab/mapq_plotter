086520e96fc834db9710c1d064e66d38a1583042
Eric Kofman <ericrkofman@gmail.com>
2019-03-25 16:31:12 -0400

    Output dir specification

--- mapq_around_locus/mapq_around_locus.py
+++ mapq_around_locus/mapq_around_locus.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
-#matplotlib.use('Agg')
 import argparse
-import pysam
 import subprocess
 import sys
 import math
@@ -30,11 +28,10 @@ def mapq_around_locus(bam_file, chromosome, position, window, offset):
             quality_scores.append(quality)
         except:
             sys.stderr.write("Couldn't get quality score for {}\n".format(line.split('\t')))
-    #quality_scores = random.sample(range(0, 60), 30)
     return pd.DataFrame(quality_scores, columns=[offset])
 
 
-def plot_mapq_around_locus(bam_file, chromosome, position, window, slide, max_distance):
+def plot_mapq_around_locus(bam_file, chromosome, position, window, slide, max_distance, output_dir='.'):
     """Plot the distribution of read quality scores"""
     dfs = []
     for offset in range(-max_distance, max_distance+slide, slide):
@@ -53,15 +50,18 @@ def plot_mapq_around_locus(bam_file, chromosome, position, window, slide, max_di
     plt.xticks(size=8)
     plt.ylabel('MapQ Score')
     plt.xlabel('Offset (base pairs)')
-    plt.title('Read MAPQ quantiles in {} bp range around {}:{}'.format(max_distance*2, chromosome, position))
+    plt.title('Read MAPQ quantiles in {} bp range around {}:{}\nWindow size: {}'.format(max_distance*2,
+                                                                                        chromosome,
+                                                                                        position,
+                                                                                        window))
 
     quantiles_values = ['20%', '40%', '60%', '80%', '100%']
     quantiles_df.index = pd.Index(quantiles_values)
 
     quantiles_df = quantiles_df.T
 
-    plt.plot(quantiles_df)
-    plt.savefig('{}:{}.png'.format(chromosome, position))
+    plt.plot(quantiles_df, marker='.')
+    plt.savefig('{}/{}:{}.png'.format(output_dir, chromosome, position))
     plt.figure()
 
 
@@ -74,6 +74,7 @@ def main():
     parser.add_argument('--window', metavar='window', type=int, default=100)
     parser.add_argument('--slide', metavar='slide', type=int, default=50)
     parser.add_argument('--max_distance', metavar='max_distance', type=int, default=400)
+    parser.add_argument('--output_dir', metavar='output_dir', type=str, default='.')
     args = parser.parse_args()
 
     bam_file = args.bam_file
@@ -83,6 +84,7 @@ def main():
     window = args.window
     slide = args.slide
     max_distance = args.max_distance
+    output_dir = args.output_dir
 
     if positions_file:
         # If a positions file is specified, output plots for each of the positions listed in the file
@@ -91,10 +93,10 @@ def main():
             row = row[1]
             chromosome = row.Chromosome
             position = row.Start
-            plot_mapq_around_locus(bam_file, chromosome, position, window, slide, max_distance)
+            plot_mapq_around_locus(bam_file, chromosome, position, window, slide, max_distance, output_dir)
     else:
         # If a positions file is not specified, but chromosome and position are specified, just do a one-time plotting:
-        plot_mapq_around_locus(bam_file, chromosome, position, window, slide, max_distance)
+        plot_mapq_around_locus(bam_file, chromosome, position, window, slide, max_distance, output_dir)
 
 
 if __name__ == '__main__':
